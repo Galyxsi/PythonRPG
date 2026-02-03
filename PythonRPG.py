5870fd2837c25a0ee07d7a02ab1426ab0bd5f4d7
Sylver Galyxsi <superpikaluke@gmail.com>
2026-02-03 07:21:59 -0600

    Image location reorganized Added text coloring (for whole text, not mid-text) Added Snake arcade cabinet

--- PythonRPG.py
+++ PythonRPG.py
@@ -56,7 +56,7 @@ class Font:
         ":cWhite:": {255,255,255},
     }
     def __init__(self, image_path, char_w, char_h, chars, cols):
-        self.sheet = pygame.image.load(image_path).convert_alpha()
+        self.sheet = pygame.image.load("sprites/ui/font/" + image_path).convert_alpha()
         self.char_w = char_w
         self.char_h = char_h
         self.cols = cols
@@ -84,6 +84,11 @@ class Font:
                 continue
             image = self.glyphs.get(char, self.fallback)
             image.set_alpha(color[3])
+            if color != (255, 255, 255, 255):
+                for xi in range(image.get_width()):
+                    for yi in range (image.get_height()):
+                        if image.get_at((xi, yi)) != (0, 0, 0, 0):
+                            image.set_at((xi, yi), (color[0], color[1], color[2], image.get_at((xi, yi))[3]))
             #if i == 
             if scale == 1:
                 surface.blit(image, (cx, cy))
@@ -367,34 +372,120 @@ class Map:
         
 class Snake():
     snakeBody = []
+    validLocations = []
 
-    def __init__():
+    snakeColors = [
+        [(0,90,0), (50,200,50), (255,0,0), (255,255,255), (0,0,0)],
+        [(95,123,118), (135,157,163), (95,123,118), (135,157,163), (50,57,70)],
+    ]
+
+    font = Font("arcadefont.png", 3,4, "1234567890", 5)
+
+    def __init__(self):
         self.snakeBody = []
+        for i in range(32 * 32):
+            self.validLocations.append(i)
+        self.snakeBodyLength = 5
         self.snakeHead = (16, 16)
         self.apple = (random.randint(0,31), random.randint(0,31))
-        self.dir = 0
+        self.dir = -1
+        self.curDir = -1
+        self.started = False
+        self.textFade = 0
+
+        #mini_snake_screen.fill((255,255,255))
+        #pygame.draw.rect(mini_snake_screen, (0,0,0), (self.snakeHead[0], self.snakeHead[1], 1, 1))
+        #for body in self.snakeBody:
+        #    pygame.draw.rect(mini_snake_screen, (0,0,0), (body[0], body[1], 1, 1))
+        #pygame.draw.rect(mini_snake_screen, (0,0,0), (self.apple[0], self.apple[1], 1, 1))
     
-    def update(self, input):
-        if pygame.key.get_pressed()[pygame.K_w]:
-            dir = 1
-        elif pygame.key.get_pressed()[pygame.K_a]:
-            dir = 2
-        elif pygame.key.get_pressed()[pygame.K_s]:
-            dir = 3
-        elif pygame.key.get_pressed()[pygame.K_d]:
-            dir = 0
+    def reset(self):
+        self.snakeBody = []
+        self.snakeBodyLength = 5
+        self.snakeHead = (16, 16)
+        self.apple = (random.randint(0,31), random.randint(0,31))
+        self.dir = -1
+        self.curDir = -1
+        self.started = False
 
-        if dir == 0:
-            self.snakeHead = (self.snakeHead + 1, self.snakeHead)
-        elif dir == 1:
-            self.snakeHead = (self.snakeHead, self.snakeHead - 1)
-        elif dir == 2:
-            self.snakeHead = (self.snakeHead - 1, self.snakeHead)
-        elif dir == 3:
-            self.snakeHead = (self.snakeHead, self.snakeHead + 1)
+    def update(self, frame, input):
 
-        mini_snake_screen.blit(pygame.Rect(0,0,1,1), self.snakeHead)
-        mini_snake_screen.blit(pygame.Rect(0,0,1,1), self.apple)
+        if input[0] and self.curDir != 3:
+            self.started = True
+            self.dir = 1
+        elif input[1] and self.curDir != 0:
+            self.started = True
+            self.dir = 2
+        elif input[2] and self.curDir != 1:
+            self.started = True
+            self.dir = 3
+        elif input[3] and self.curDir != 2:
+            self.started = True
+            self.dir = 0
+
+        if frame % 3 != 0 or not self.started:
+            return
+
+        self.curDir = self.dir
+
+        self.snakeBody.append(self.snakeHead)
+        if len(self.snakeBody) > self.snakeBodyLength:
+            self.snakeBody.pop(0)
+
+        if self.dir == 0:
+            self.snakeHead = (self.snakeHead[0] + 1, self.snakeHead[1])
+        elif self.dir == 1:
+            self.snakeHead = (self.snakeHead[0], self.snakeHead[1] - 1)
+        elif self.dir == 2:
+            self.snakeHead = (self.snakeHead[0] - 1, self.snakeHead[1])
+        elif self.dir == 3:
+            self.snakeHead = (self.snakeHead[0], self.snakeHead[1] + 1)
+
+        for body in self.snakeBody:
+            if self.snakeHead == body:
+                self.reset()
+
+        if self.snakeHead[0] < 0:
+            self.snakeHead = (31, self.snakeHead[1])
+        elif self.snakeHead[0] > 31:
+            self.snakeHead = (0, self.snakeHead[1])
+        elif self.snakeHead[1] < 0:
+            self.snakeHead = (self.snakeHead[0], 31)        
+        elif self.snakeHead[1] > 31:
+            self.snakeHead = (self.snakeHead[0], 0)
+
+        if self.snakeHead == self.apple:
+            for i in range(32 * 32):
+                self.validLocations.append(i)
+            for body in self.snakeBody:
+                self.validLocations.pop(body[0] + body[1] * 32)
+            appleLocation = random.choice(self.validLocations)
+            self.apple = (appleLocation % 32, appleLocation // 32)
+            self.snakeBodyLength = self.snakeBodyLength + 1
+            self.textFade = 60
+            
+        
+
+    def draw(self, screen):
+        color_index = 1
+        color1 = self.snakeColors[color_index][0]
+        color2 = self.snakeColors[color_index][1]
+        applecolor = self.snakeColors[color_index][2]
+        text_color = self.snakeColors[color_index][3]
+        screen.fill(self.snakeColors[color_index][4])
+
+        if self.textFade > 0:
+            self.font.draw(screen, str(self.snakeBodyLength - 5), 1, 1, 1, (text_color[0],text_color[1],text_color[2],255 * (self.textFade / 60)), 1)
+            self.textFade -= 1
+
+        pygame.draw.rect(screen, color1, (self.snakeHead[0], self.snakeHead[1], 1, 1))
+        for i, body in enumerate(self.snakeBody):
+            pygame.draw.rect(screen, (color2[0] - (color2[0] - color1[0]) * (i / len(self.snakeBody)), color2[1] - (color2[1] - color1[1]) * (i / len(self.snakeBody)), color2[2] - (color2[2] - color1[2]) * (i / len(self.snakeBody))), (body[0], body[1], 1, 1))
+        pygame.draw.rect(screen, applecolor, (self.apple[0], self.apple[1], 1, 1))
+
+        
+
+        pass
 
 
 
@@ -403,7 +494,7 @@ clock = pygame.time.Clock()
 
 spr_selected_tile = pygame.image.load('selectedtile.png').convert_alpha()
 
-sprsh_tileset = Spritesheet('bgtiles.png', 16, 16)
+sprsh_tileset = Spritesheet('sprites/tilesets/bgtiles.png', 16, 16)
 
 markers_full = Spritesheet('markers.png', 16, 32)
 markers_icons = Spritesheet('markers.png', 16, 16)
@@ -419,9 +510,16 @@ frame = 0
 
 cur_map = Map.load("!default_test_map")
 
+cabinet = pygame.image.load("sprites/arcades/snake_cabinet.png").convert_alpha()
+cabinet_curPos = (0,256)
+cabinet_pos = (0,256)
+
+snek = Snake()
+
 while True:
 
-    pygame.display.set_icon(game_screen)
+    pygame.display.set_icon(mini_snake_screen)
+    cabinet_curPos = (cabinet_curPos[0] + (cabinet_pos[0] - cabinet_curPos[0]) * 0.1, cabinet_curPos[1] + (cabinet_pos[1] - cabinet_curPos[1]) * 0.1)
 
 
     marker.draw(5,5)
@@ -431,8 +529,10 @@ while True:
     scaled_screen = pygame.transform.scale(game_screen, real_screen.get_size())
     if real_screen.get_width() > real_screen.get_height():
         scaled_screen = pygame.transform.scale(game_screen, (real_screen.get_height(), real_screen.get_height()))
+        scaled_snake = pygame.transform.scale(mini_snake_screen, (real_screen.get_height() // 2, real_screen.get_height() // 2))
     else:
         scaled_screen = pygame.transform.scale(game_screen, (real_screen.get_width(), real_screen.get_width()))
+        scaled_snake = pygame.transform.scale(mini_snake_screen, (real_screen.get_width() // 2, real_screen.get_width() // 2))
 
     mouse_x = (pygame.mouse.get_pos()[0] - (real_screen.get_width() - scaled_screen.get_size()[0]) / 2) * game_width / scaled_screen.get_size()[0] 
     mouse_y = (pygame.mouse.get_pos()[1] - (real_screen.get_height() - scaled_screen.get_size()[1]) / 2) * game_height / scaled_screen.get_size()[1]
@@ -464,8 +564,13 @@ while True:
                 elif event.key == pygame.K_5:
                     Debug.map_tile = 5
                     Debug.debug_print("Set tile to [" + Map.tiles[5]["name"] + "]")
-                elif event.key == pygame.K_s:
+                elif event.key == pygame.K_F1:
                     cur_map.save("_temp_save", "Local Save")
+                elif event.key == pygame.K_F2:
+                    if cabinet_pos == (0, 0):
+                        cabinet_pos = (0, 256)
+                    else:
+                        cabinet_pos = (0, 0)
         if event.type == pygame.MOUSEWHEEL:
             if Debug.map_editor:
                 Debug.map_tile += event.y
@@ -487,7 +592,10 @@ while True:
             cur_map.set(round((mouse_x + camera_x) // 16), round((mouse_y + camera_y) // 16), -1)
         else:
             game_screen.blit(sprsh_tileset.get_image(Debug.map_tile), spr_selected_tile.get_rect(center=(round((mouse_x - 8 + camera_x % 16) / 16) * 16 + 8 - camera_x % 16, round((mouse_y - 8 + camera_y % 16) / 16) * 16 + 8 - camera_y % 16)))
-    
+            #game_screen.blit(pygame.transform.scale(game_screen, (game_screen.get_size()[0] // 16, game_screen.get_size()[1] // 16)), spr_selected_tile.get_rect(center=(round((mouse_x - 8 + camera_x % 16) / 16) * 16 + 8 - camera_x % 16, round((mouse_y - 8 + camera_y % 16) / 16) * 16 + 8 - camera_y % 16)))
+            #game_screen.blit(real_screen, spr_selected_tile.get_rect(center=(round((mouse_x - 8 + camera_x % 16) / 16) * 16 + 8 - camera_x % 16, round((mouse_y - 8 + camera_y % 16) / 16) * 16 + 8 - camera_y % 16)))
+        
+
     if pygame.key.get_pressed()[pygame.K_LSHIFT] or pygame.key.get_pressed()[pygame.K_RSHIFT]:
         cam_speed = 4
     else:
@@ -512,7 +620,15 @@ while True:
 
     clock.tick(60)
 
-    
+    game_screen.blit(cabinet, cabinet_curPos)
 
     real_screen.blit(scaled_screen, scaled_screen.get_rect(center=real_screen.get_rect().center))
+
+    if round(cabinet_curPos[1]) == 0:
+        snek.update(frame, (pygame.key.get_pressed()[pygame.K_w], pygame.key.get_pressed()[pygame.K_a], pygame.key.get_pressed()[pygame.K_s], pygame.key.get_pressed()[pygame.K_d]))
+        snek.draw(mini_snake_screen)
+        real_screen.blit(scaled_snake, scaled_snake.get_rect(center=real_screen.get_rect().center))
+
+    
+    
     pygame.display.flip()
---
+++ roadmap.md
@@ -0,0 +1,42 @@
+# Project Roadmap — Python RPG
+
+Nothing here's exactly a guarentee, but rather a hopeful look at what I hope to make by the end.
+
+## Phase 1: Maps & Editor
+
+- Savable and loadable maps
+- Tile Types & Visual Tiles
+- Movable camera
+- Configurable map size
+
+## Phase 2: Units
+
+- Placeable objects (markers, items, interactables, visual objects)
+- Placeable characters/units
+- Selectable tile formatting
+- Simple character moving
+
+## Phase 3: Gameplay Loop
+
+- Character actions
+- Character data (HP, stats, etc.)
+
+- Simple opponent AI
+- Win/Lose Conditions
+
+- Simple equipment + action modifiers
+- Simple items
+
+- Menu with selectable senarios/maps
+
+## Phase 4: Data & Content
+
+- Datadriven characters, items, equipment, maps, etc.
+- Loadable per scenario/map
+- Variety of selectable example battle senario
+
+## Phase M: Mod Support (Optional)
+
+- mods/ folder
+- Loadable as an alternative filepath for characters, objects, sprites, data, etc.
+
